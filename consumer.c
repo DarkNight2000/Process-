d0f0fa71a9b7b9458c724a9db1abd2a8eca45b00
DarkNight2000 <zlf2010001@gmail.com>
2016-10-19 15:32:59 -0400

    Add files via upload

---
+++ consumer.c
@@ -0,0 +1,37 @@
+/********* consumer.c ***********/
+#include "headers.h"
+
+int main()
+{
+	const char *name = "OS";
+    int shm_fd;
+    shared_struct *ptr;
+	int size, i;
+	FILE * pFile = fopen ("output.txt","w");
+	if (pFile == NULL) {
+		printf("fopen failed\n");
+		exit(-1);
+	}
+    
+	/* open the shared memory segment as if it was a file */
+	shm_fd = shm_open(name, O_RDWR, 0666);
+	if (shm_fd == -1) {
+		printf("Shared memory failed\n");
+		exit(-1);
+	}
+    
+	/* the size of the shared memory segment */
+	size = sizeof(shared_struct);
+
+    /* map the shared memory segment to the address space of the process */
+    ptr = mmap(0, size, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (ptr == MAP_FAILED) {
+        printf("Map failed\n");
+        exit(-1);
+    }
+
+    /* Read data and write to file output.txt */
+    
+	fclose(pFile);
+	shm_unlink(name);
+}
